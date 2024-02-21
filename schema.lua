f6d2727dbe3b72ef2c7623f931c4386c67bd8c86
anhdt34 <157108309+anhdt34@users.noreply.github.com>
2024-02-21 14:34:34 +0700

    Update schema.lua

--- schema.lua
+++ schema.lua
@@ -1,6 +1,5 @@
 local typedefs = require "kong.db.schema.typedefs"
 
-
 return {
   name = "kong-oauth2-custom",
   fields = {
@@ -10,8 +9,8 @@ return {
         type = "record",
         fields = {
           { authorization_endpoint = typedefs.url({ required = true }) },
-          { token_header = typedefs.header_name { default = "Authorization", required = true }, },
-          { user_id_header = typedefs.user_id_header { default = "X-User-Id", required = true }, }
+          { token_header = typedefs.header_name { default = "Authorization" } },
+          { user_id_header = typedefs.header_name { default = "X-User-Id" } }
         }, 
       }, 
     },
